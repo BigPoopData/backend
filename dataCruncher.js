f75231a5a0cfe134903bb2df0d683ccd65fb3322
MeksPwnschlager <doctoranyk@gmail.com>
2017-02-07 19:07:56 +0100

    init

---
+++ dataCruncher.js
@@ -0,0 +1,194 @@
+/*
+  Toilet Data Cruncher
+
+  I use a lot of callbacks here because I want to be
+  able to wait for all the maths to finish
+*/
+
+(function() {
+
+  var computeAverageClosedIntervalDurationPerWeekday = function(toilet, cb) {
+    let closedIntervals = toilet.closedIntervals;
+    let averageClosedIntervalsPerWeekday = new Array();
+    for (var i = 0; i < 7; i++) // set up array to be filled (7 Weekdays)
+      averageClosedIntervalsPerWeekday.push({average: 0, intervals: 0});
+
+    closedIntervals.forEach(function (item) { // 0: Sunday, 1: Monday, 2: Tuesday...
+      averageClosedIntervalsPerWeekday[item.from.getDay()].average += item.duration;
+      averageClosedIntervalsPerWeekday[item.from.getDay()].intervals ++;
+    });
+    averageClosedIntervalsPerWeekday.forEach(function (item) {
+      item.average = item.average / item.intervals;
+    });
+    cb(averageClosedIntervalsPerWeekday);
+  }
+
+  var computeAverageClosedIntervalDurationPerHour = function(toilet, cb) {
+    let closedIntervals = toilet.closedIntervals;
+    let averageClosedIntervalsPerHour = new Array();
+    for (var i = 0; i < 24; i++) // set up array to be filled
+      averageClosedIntervalsPerHour.push({average: 0, intervals: 0});
+
+    closedIntervals.forEach(function (item) {
+      averageClosedIntervalsPerHour[item.from.getHours()].average += item.duration;
+      averageClosedIntervalsPerHour[item.from.getHours()].intervals ++;
+    });
+    averageClosedIntervalsPerHour.forEach(function (item) {
+      item.average = item.average / item.intervals;
+    });
+    cb(averageClosedIntervalsPerHour);
+  }
+
+    var computeClosedOpenRatio = function(toilet, cb){
+      let open = toilet.openIntervals;
+      let closed = toilet.closedIntervals;
+      let totalOpen = 0;
+      let totalClosed = 0;
+      let result = {openPercentage: 0, closedPercentage: 0}
+      for(i = 0; i < open.length; i++) {
+        totalOpen += open[i].duration;
+      }
+      for(i = 0; i < closed.length; i++) {
+        totalClosed += closed[i].duration;
+      }
+      let total = (totalClosed + totalOpen);
+      result.openPercentage = Math.floor((totalOpen / total)*100);
+      result.closedPercentage = Math.ceil((totalClosed / total)*100);
+
+      cb(result);
+    }
+
+
+    var onlyUnique = function(value, index, self) {
+        return self.indexOf(value) === index;
+    }
+
+    var computeDistinctDays = function(data) {
+      let stamps = new Array();
+      for(i = 0; i < data.length; i++) {
+        let date = new Date(Date.parse(data[i].timestamp))
+        stamps.push(date.toDateString());
+      }
+      stamps = stamps.filter(onlyUnique);
+      return stamps;
+    }
+
+    var computeAverageClosedIntervalDurationPerDay = function(toilet, cb) {
+      let closedIntervals = toilet.closedIntervals;
+      let stamps = computeDistinctDays(toilet.toiletEvents);
+      let averageClosedIntervalsPerDay = new Array();
+
+
+      for (var i = 0; i < stamps.length; i++) // set up array to be filled
+        averageClosedIntervalsPerDay.push({timestamp: null, average: 0, intervals: 0});
+
+      for(i = 0; i < closedIntervals.length; i++) {
+        for(j = 0; j < stamps.length; j++) {
+          if (stamps[j] == closedIntervals[i].from.toDateString()) {
+            averageClosedIntervalsPerDay[j].timestamp = stamps[j];
+            averageClosedIntervalsPerDay[j].average += closedIntervals[i].duration;
+            averageClosedIntervalsPerDay[j].intervals ++;
+          }
+        }
+      }
+      for(i = 0; i < averageClosedIntervalsPerDay.length; i++) {
+        let average = averageClosedIntervalsPerDay[i].average / averageClosedIntervalsPerDay[i].intervals;
+        averageClosedIntervalsPerDay[i].average = average;
+      }
+      cb(averageClosedIntervalsPerDay);
+    }
+
+    var computeIntervals = function(data, closed, cb) {
+      let intervals = new Array();
+      let lastTime = null;
+
+      for(i = 0; i < data.length; i++) {
+        if (closed == (data[i].open == 0)) { // Boolean to use Function to compute both Interval types
+          lastTime = data[i].timestamp;
+        } else if (lastTime) {
+          let item = {from: lastTime, duration:((data[i].timestamp - lastTime) / 1000)};
+          if (item.duration > 0){
+            if (closed){
+              if (1800 > item.duration) intervals.push(item); //More Crapdata Elimination
+            } else {
+              intervals.push(item);
+            }
+          }
+        }
+      }
+      cb(intervals);
+    }
+
+    var compileDataForComputation = function(db, cb) {
+      let result = new Array();
+      db.each(`SELECT * FROM sitzklo_log ORDER BY datetime("timestamp");`, function(err, row) {
+        //console.log(err);
+        row.timestamp = new Date(Date.parse(row.timestamp));
+        //console.log(row);
+        result.push(row);
+      }, function (err, number) {
+        if (err) console.log(err);
+        let toilet = {
+          toiletEvents: result,
+          openIntervals: new Array(),
+          closedIntervals: new Array(),
+        }
+        db.each(`SELECT * FROM sitzklo_closed_intervals ORDER BY datetime("from");`, function(err, row) {
+          row.from = new Date(Date.parse(row.from));
+          toilet.closedIntervals.push(row);
+        }, function (err, numRows) {
+          if (err) console.log(err);
+          db.each(`SELECT * FROM sitzklo_open_intervals ORDER BY datetime("from");`, function(err, row) {
+            row.from = new Date(Date.parse(row.from));
+            toilet.openIntervals.push(row);
+          },function (err, numRows) {
+            if (err) console.log(err);
+            cb(toilet);
+          });
+        });
+      });
+    }
+
+    var compileDataJson = function (db, cb) {
+      let data = {
+        closedOpenRatio: null,
+        averageClosedDurationPerDay: null,
+        averageClosedDurationPerHour: null,
+        averageClosedDurationPerWeekday: null,
+        totalIntervals: null, // TODO
+        averageIntervalsPerHour: null, //TODO
+        averageIntervalsPerWeekday: null, //TODO
+        bigSmallRatio: null, //TODO
+        estimatedWaterUsage: null, //TODO
+      }
+
+      // Computation Callback Chain
+      compileDataForComputation(db, function(toilet) {
+        //console.log(toilet);
+        computeClosedOpenRatio(toilet, function(closedOpenRatio){
+          data.closedOpenRatio = closedOpenRatio;
+          computeAverageClosedIntervalDurationPerDay(toilet, function(avgDursPerDay) {
+            data.averageClosedDurationPerDay = avgDursPerDay;
+            computeAverageClosedIntervalDurationPerHour(toilet, function(avgDursPerHour) {
+              data.averageClosedDurationPerHour = avgDursPerHour;
+              computeAverageClosedIntervalDurationPerWeekday(toilet, function(avgDursPerWeekday) {
+                data.averageClosedDurationPerWeekday = avgDursPerWeekday;
+                cb(JSON.stringify(data));
+              });
+            });
+          });
+        });
+      });
+    }
+
+    var DataCruncher = function() {
+
+    }
+
+    DataCruncher.prototype = {
+      compileDataJson: compileDataJson
+    };
+
+    module.exports = DataCruncher;
+
+}());
---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ews = require('express-ws')(express());
+const app = ews.app;
+
+const dc = require('./dataCruncher');
+const cruncher = new dc();
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./toilet.db');
+
+var lastEvent = {timestamp: null, open: null};
+var connections = new Array();
+
+function handleWsMessage(msg, ws){
+  switch (msg) {
+    case "setup":
+      console.log("Sending Setup Object");
+      cruncher.compileDataJson(db, function(dataString) {
+        console.log(dataString);
+        ws.send(dataString);
+      });
+      break;
+    default:
+      console.log("Invalid Command: " + msg);
+  }
+}
+
+
+function processToiletEvent(event) {
+  if (lastEvent.timestamp != null){
+    let newInterval = {from: lastEvent.timestamp.toString().slice(0,24), duration: ((event.timestamp - lastEvent.timestamp) / 1000)}
+
+    if ((lastEvent.open == "true") && (event.open != "true")) {
+      if ((60 * 60 * 24) > newInterval.duration > 0) { // If Open Interval longer than one day, throw Interval away
+        db.run(`INSERT INTO sitzklo_open_intervals ([from], duration) VALUES($1, $2);`, [newInterval.from, newInterval.duration]);
+        db.run(`INSERT INTO sitzklo_log (timestamp, open) VALUES($1, $2);`, [event.timestamp.toString().slice(0,24), event.open]);
+        lastEvent = event;
+        console.log("Inserted Interval:" + newInterval);
+      }
+    } else if((lastEvent.open == "false" ) && (event.open != "false")) {
+      if (1800 > newInterval.duration > 0){ // If Closed Interval longer than one 50 minutes, throw Interval away, nobody willl shit that long
+        db.run(`INSERT INTO sitzklo_closed_intervals ([from], duration) VALUES($1, $2);`, [newInterval.from, newInterval.duration]);
+        db.run(`INSERT INTO sitzklo_log (timestamp, open) VALUES($1, $2);`, [event.timestamp.toString().slice(0,24), event.open]);
+        lastEvent = event;
+        console.log("Inserted Interval:" + JSON.stringify(newInterval));
+      }
+    }
+  } else {
+    lastEvent = event;
+  }
+}
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.ws('/ws', function(ws, req) {
+  ws.on('message', function(msg) {
+    handleWsMessage(msg, ws);
+  });
+  connections.push(ws)
+  console.log("New Websocket Connection");
+});
+
+app.get('/:klo/open/:status', function(req, res, next){
+  let newEvent = {name: req.params.klo, timestamp: new Date(), open: req.params.status};
+  console.log(newEvent.timestamp.toString() + " /" + req.params.klo + "/open/" + newEvent.open);
+  if ((newEvent.open == "true") || (newEvent.open == "false")) { // Paranoid in the wrong places
+    processToiletEvent(newEvent);
+    connections.forEach(function(ws) {
+      ws.send(JSON.stringify(newEvent));
+    });
+  }
+  res.end();
+});
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log('---- Backend Started ----');
+});

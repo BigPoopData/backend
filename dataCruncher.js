698140b072dec78691f23cfd105602069881c8c2
MeksPwnschlager <doctoranyk@gmail.com>
2017-02-11 21:14:39 +0100

    refactored cruncher, implemented lst event logic

--- dataCruncher.js
+++ dataCruncher.js
@@ -7,6 +7,14 @@
 
 (function() {
 
+  var getLastEvent = function(db, cb) {
+    db.get(`SELECT * FROM sitzklo_current;`, function(err, lastDbEvent) {
+      if (err) console.log(err);
+      lastDbEvent.timestamp = new Date(Date.parse(lastDbEvent.timestamp));
+      cb(lastDbEvent);
+    });
+  }
+
   var computeAverageClosedIntervalDurationPerWeekday = function(toilet, cb) {
     let closedIntervals = toilet.closedIntervals;
     let averageClosedIntervalsPerWeekday = new Array();
@@ -151,6 +159,8 @@
 
     var compileDataJson = function (db, cb) {
       let data = {
+        name: "FullObject",
+        lastEvent: null,
         closedOpenRatio: null,
         averageClosedDurationPerDay: null,
         averageClosedDurationPerHour: null,
@@ -164,19 +174,49 @@
 
       // Computation Callback Chain
       compileDataForComputation(db, function(toilet) {
-        computeClosedOpenRatio(toilet, function(closedOpenRatio){
-          data.closedOpenRatio = closedOpenRatio;
-          computeAverageClosedIntervalDurationPerDay(toilet, function(avgDursPerDay) {
-            data.averageClosedDurationPerDay = avgDursPerDay;
-            computeAverageClosedIntervalDurationPerHour(toilet, function(avgDursPerHour) {
-              data.averageClosedDurationPerHour = avgDursPerHour;
-              computeAverageClosedIntervalDurationPerWeekday(toilet, function(avgDursPerWeekday) {
-                data.averageClosedDurationPerWeekday = avgDursPerWeekday;
-                cb(JSON.stringify(data));
+        getLastEvent(db, function(lastDbEvent) {
+          data.lastEvent = lastDbEvent;
+          computeClosedOpenRatio(toilet, function(closedOpenRatio){
+            data.closedOpenRatio = closedOpenRatio;
+            computeAverageClosedIntervalDurationPerDay(toilet, function(avgDursPerDay) {
+              data.averageClosedDurationPerDay = avgDursPerDay;
+              computeAverageClosedIntervalDurationPerHour(toilet, function(avgDursPerHour) {
+                data.averageClosedDurationPerHour = avgDursPerHour;
+                computeAverageClosedIntervalDurationPerWeekday(toilet, function(avgDursPerWeekday) {
+                  data.averageClosedDurationPerWeekday = avgDursPerWeekday;
+                  cb(JSON.stringify(data));
+                });
               });
             });
           });
         });
+      });
+    }
+
+
+    var processToiletEvent = function(event, db) {
+      getLastEvent(db, function (lastEvent){
+        if (lastEvent.timestamp != null){
+          let newInterval = {from: lastEvent.timestamp.toString().slice(0,24), duration: ((event.timestamp - lastEvent.timestamp) / 1000)}
+
+          if ((lastEvent.open == "true") && (event.open != "true")) {
+            if ((60 * 60 * 24) > newInterval.duration > 0) { // If Open Interval longer than one day, throw Interval away
+              db.run(`INSERT INTO sitzklo_open_intervals ([from], duration) VALUES($1, $2);`, [newInterval.from, newInterval.duration]);
+              db.run(`INSERT INTO sitzklo_log (timestamp, open) VALUES($1, $2);`, [event.timestamp.toString().slice(0,24), event.open]);
+                      console.log("Inserted Interval:" + JSON.stringify(newInterval));
+            }
+          } else if((lastEvent.open == "false" ) && (event.open != "false")) {
+            if (1800 > newInterval.duration > 0){ // If Closed Interval longer than one 50 minutes, throw Interval away, nobody willl shit that long
+              db.run(`INSERT INTO sitzklo_closed_intervals ([from], duration) VALUES($1, $2);`, [newInterval.from, newInterval.duration]);
+              db.run(`INSERT INTO sitzklo_log (timestamp, open) VALUES($1, $2);`, [event.timestamp.toString().slice(0,24), event.open]);
+              console.log("Inserted Interval:" + JSON.stringify(newInterval));
+            }
+          }
+          db.run(`UPDATE sitzklo_current
+                  SET timestamp = $1, open = $2
+                  WHERE (id = 0)`, [event.timestamp.toString().slice(0,24), event.open]);
+          console.log("Updated Last Event");  
+        }
       });
     }
 
@@ -185,7 +225,8 @@
     }
 
     DataCruncher.prototype = {
-      compileDataJson: compileDataJson
+      compileDataJson: compileDataJson,
+      processToiletEvent: processToiletEvent
     };
 
     module.exports = DataCruncher;
--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const cruncher = new dc();
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./toilet.db');
 
-var lastEvent = {timestamp: null, open: null};
 var connections = new Array();
 
 function handleWsMessage(msg, ws){
@@ -26,29 +25,16 @@ function handleWsMessage(msg, ws){
   }
 }
 
-
-function processToiletEvent(event) {
-  if (lastEvent.timestamp != null){
-    let newInterval = {from: lastEvent.timestamp.toString().slice(0,24), duration: ((event.timestamp - lastEvent.timestamp) / 1000)}
-
-    if ((lastEvent.open == "true") && (event.open != "true")) {
-      if ((60 * 60 * 24) > newInterval.duration > 0) { // If Open Interval longer than one day, throw Interval away
-        db.run(`INSERT INTO sitzklo_open_intervals ([from], duration) VALUES($1, $2);`, [newInterval.from, newInterval.duration]);
-        db.run(`INSERT INTO sitzklo_log (timestamp, open) VALUES($1, $2);`, [event.timestamp.toString().slice(0,24), event.open]);
-        lastEvent = event;
-        console.log("Inserted Interval:" + JSON.stringify(newInterval));
-      }
-    } else if((lastEvent.open == "false" ) && (event.open != "false")) {
-      if (1800 > newInterval.duration > 0){ // If Closed Interval longer than one 50 minutes, throw Interval away, nobody willl shit that long
-        db.run(`INSERT INTO sitzklo_closed_intervals ([from], duration) VALUES($1, $2);`, [newInterval.from, newInterval.duration]);
-        db.run(`INSERT INTO sitzklo_log (timestamp, open) VALUES($1, $2);`, [event.timestamp.toString().slice(0,24), event.open]);
-        lastEvent = event;
-        console.log("Inserted Interval:" + JSON.stringify(newInterval));
-      }
+function sendWsMessage(msg) {
+  connections.forEach(function(ws, index) {
+    if (ws.readyState == ws.OPEN){ //check if connection active
+      ws.send(msg, function(err){
+        if (err) console.log(err);
+      });
+    } else {
+      connections.splice(index, 1); //remove connection if no longer active
     }
-  } else {
-    lastEvent = event;
-  }
+  });
 }
 
 //Express
@@ -69,17 +55,8 @@ app.get('/:klo/open/:status', function(req, res, next){
   console.log(newEvent.timestamp.toString() + " /" + req.params.klo + "/open/" + newEvent.open);
 
   if ((newEvent.open == "true") || (newEvent.open == "false")) { // Paranoid in the wrong places
-    processToiletEvent(newEvent);
-
-    connections.forEach(function(ws, index) {
-      if (ws.readyState == ws.OPEN){ //check if connection active
-        ws.send(JSON.stringify(newEvent), function(err){
-          if (err) console.log(err);
-        });
-      } else {
-        connections.splice(index, 1); //remove connection if no longer active
-      }
-    });
+    cruncher.processToiletEvent(newEvent, db);
+    sendWsMessage(JSON.stringify(newEvent));
   }
 
   res.end();

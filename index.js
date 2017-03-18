734715b9528bda0a62e82b2b2348e92e46a3e62f
MeksPwnschlager <doctoranyk@gmail.com>
2017-03-18 20:20:36 +0100

    prevent browser from caching

--- index.js
+++ index.js
@@ -54,7 +54,13 @@ function broadcastWsMessage(msg) {
 }
 
 //Express
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+        res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+        res.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+        res.setHeader("Expires", "0"); // Proxies.
+        next();
+    })
+    .use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
